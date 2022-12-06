ffffd21f81cc2a3557aa93ba0467cc9863524fe3
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-06 09:58:31 +0530

    add student

--- src/main/java/com/example/studentapp2_backend/controller/StudentController.java
+++ src/main/java/com/example/studentapp2_backend/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.example.studentapp2_backend.controller;
 
+import com.example.studentapp2_backend.model.Students;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,16 +14,13 @@ public class StudentController {
     return "welcome";
 }
 
-@GetMapping("/content")
-    public String contactpage()
+@PostMapping(path="/add",consumes = "application/json", produces = "application/json")
+    public String add(@RequestBody Students s)
 {
-    return  "welcome to contactpage";
+    System.out.println(s.toString());
+    return  "student details added successfully";
 }
 
-@GetMapping("/gallary")
-    public  String gallary()
-{
-    return "WELCOME TO GALLARY PAGE";
-}
+
 }
 
