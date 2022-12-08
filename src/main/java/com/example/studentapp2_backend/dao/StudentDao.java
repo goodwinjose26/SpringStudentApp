eb88a89ff91564fb2a450796d243d91bac12db5b
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-08 16:19:14 +0530

    delete

--- src/main/java/com/example/studentapp2_backend/dao/StudentDao.java
+++ src/main/java/com/example/studentapp2_backend/dao/StudentDao.java
@@ -1,6 +1,8 @@
 package com.example.studentapp2_backend.dao;
 
 import com.example.studentapp2_backend.model.Students;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +12,11 @@ import java.util.List;
 public interface StudentDao extends CrudRepository<Students,Integer> {
     @Query(value = "SELECT `id`, `admno`, `collegename`, `name`, `rollno` FROM `students` WHERE `admno`=:admno",nativeQuery = true)
     List<Students> searchStudent(@Param("admno") String admno);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `students` WHERE `id`=:id",nativeQuery = true)
+    void deleteStudent(@Param("id") Integer id);
+
 }
